c44dd01d25b7a352fcc2821306bd9f52deaf2a3b
Phylip Genty <phylipgenty@gmail.com>
2026-04-08 13:32:01 +0100

    The images should appear now

--- ppn_project/urls.py
+++ ppn_project/urls.py
@@ -8,17 +8,6 @@ urlpatterns = [
     path('', include('core.urls')),
 ]
 
-# Serve media files during development
+# Serve media files during development (or always on Render for testing)
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    # ... your existing urlpatterns ...
-]
-
-# Serve media files in production (Render) – not ideal but works
-if settings.DEBUG or not settings.DEBUG:  # serve always
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
