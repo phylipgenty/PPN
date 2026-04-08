cc71f2dd916f3fb414ab0432cc0af5a08f7c2d6f
Phylip Genty <phylipgenty@gmail.com>
2026-04-08 13:25:37 +0100

    Fixing the upload image

--- ppn_project/urls.py
+++ ppn_project/urls.py
@@ -11,3 +11,14 @@ urlpatterns = [
 # Serve media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    # ... your existing urlpatterns ...
+]
+
+# Serve media files in production (Render) – not ideal but works
+if settings.DEBUG or not settings.DEBUG:  # serve always
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
