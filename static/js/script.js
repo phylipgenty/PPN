fb307ebf4eb748528f9a7fd317d101fcf9654db3
Phylip Genty <phylipgenty@gmail.com>
2026-04-12 13:18:18 +0300

    Floaters😂

--- static/js/script.js
+++ static/js/script.js
@@ -182,3 +182,20 @@ function startManchesterCountdown() {
   }, 1000);
 }
 startManchesterCountdown();
+
+document.querySelectorAll('.glass-card-light').forEach(card => {
+  card.addEventListener('mousemove', e => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const rotateX = ((y / rect.height) - 0.5) * -10;
+    const rotateY = ((x / rect.width) - 0.5) * 10;
+
+    card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) translateY(-10px)`;
+  });
+
+  card.addEventListener('mouseleave', () => {
+    card.style.transform = 'translateY(0)';
+  });
+});
